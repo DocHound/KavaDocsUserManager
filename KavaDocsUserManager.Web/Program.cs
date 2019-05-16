d43b46c99d1d7492e5a9afc261761aa609dc43f7
Rick Strahl <rstrahl@west-wind.com>
2019-05-16 12:50:33 -1000

    Change WebHost Startup

--- KavaDocsUserManager.Web/Program.cs
+++ KavaDocsUserManager.Web/Program.cs
@@ -14,12 +14,28 @@ namespace KavaDocsUserManager
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var builder = CreateWebHostBuilder(args);
+            builder.Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+
+            var builder = WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+            //.UseIIS()
+            //.UseHttpSys(options =>
+            //{
+            //    options.Authentication.Schemes = AuthenticationSchemes.None;
+            //    options.Authentication.AllowAnonymous = true;
+            //    options.MaxConnections = 100;
+            //    options.MaxRequestBodySize = 30000000;
+            //    options.UrlPrefixes.Add("http://localhost:5002");
+            //})
+
+
+            return builder;
+
+        }
     }
 }
