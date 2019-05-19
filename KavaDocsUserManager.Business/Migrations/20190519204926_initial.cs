02d5718dae59a8fc9d66bf8633046f9018ef5657
Rick Strahl <rstrahl@west-wind.com>
2019-05-19 10:53:21 -1000

    Clear out migrations and reset to initial.

--- KavaDocsUserManager.Business/Migrations/20190519204926_initial.cs
+++ KavaDocsUserManager.Business/Migrations/20190519204926_initial.cs
@@ -1,21 +1,33 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
-using System;
-using System.Collections.Generic;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace KavaDocsUserManager.Business.Migrations
 {
-    public partial class INITIAL : Migration
+    public partial class initial : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "AuthTokens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false),
+                    Entered = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthTokens", x => x.Id);
+                });
+
             migrationBuilder.CreateTable(
                 name: "Organizations",
                 columns: table => new
                 {
                     Id = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(maxLength: 100, nullable: true),
                     Description = table.Column<string>(maxLength: 250, nullable: true),
-                    IsActive = table.Column<bool>(nullable: false),
-                    Title = table.Column<string>(maxLength: 100, nullable: true)
+                    IsActive = table.Column<bool>(nullable: false)
                 },
                 constraints: table =>
                 {
@@ -27,15 +39,17 @@ namespace KavaDocsUserManager.Business.Migrations
                 columns: table => new
                 {
                     Id = table.Column<Guid>(nullable: false),
-                    Description = table.Column<string>(maxLength: 200, nullable: true),
-                    Domain = table.Column<string>(maxLength: 100, nullable: true),
-                    IncludeInSearchResults = table.Column<bool>(nullable: false),
-                    IsActive = table.Column<bool>(nullable: false),
                     Prefix = table.Column<string>(maxLength: 50, nullable: false),
+                    Domain = table.Column<string>(maxLength: 100, nullable: true),
+                    Title = table.Column<string>(maxLength: 100, nullable: false),
+                    Description = table.Column<string>(maxLength: 200, nullable: true),
+                    Tags = table.Column<string>(nullable: true),
                     Settings = table.Column<string>(nullable: true),
                     TableOfContents = table.Column<string>(nullable: true),
-                    Tags = table.Column<string>(nullable: true),
-                    Title = table.Column<string>(maxLength: 100, nullable: false)
+                    CssOverrides = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    RequiresAuth = table.Column<bool>(nullable: false),
+                    IncludeInSearchResults = table.Column<bool>(nullable: false)
                 },
                 constraints: table =>
                 {
@@ -43,21 +57,37 @@ namespace KavaDocsUserManager.Business.Migrations
                 });
 
             migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Level = table.Column<int>(nullable: false),
+                    IsGlobalAdmin = table.Column<bool>(nullable: false),
+                    IsRepoAdmin = table.Column<bool>(nullable: false),
+                    RepositoryId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "Users",
                 columns: table => new
                 {
                     Id = table.Column<Guid>(nullable: false),
-                    Company = table.Column<string>(maxLength: 100, nullable: true),
-                    Created = table.Column<DateTime>(nullable: false),
+                    UserDisplayName = table.Column<string>(maxLength: 50, nullable: false),
                     Email = table.Column<string>(maxLength: 150, nullable: false),
                     FirstName = table.Column<string>(maxLength: 80, nullable: true),
-                    Initials = table.Column<string>(maxLength: 5, nullable: true),
-                    IsActive = table.Column<bool>(nullable: false),
-                    IsAdmin = table.Column<bool>(nullable: false),
                     LastName = table.Column<string>(maxLength: 100, nullable: true),
-                    Password = table.Column<string>(maxLength: 80, nullable: false),
-                    UserDisplayName = table.Column<string>(maxLength: 50, nullable: false),
-                    ValidationKey = table.Column<string>(maxLength: 35, nullable: true)
+                    Initials = table.Column<string>(maxLength: 5, nullable: true),
+                    Company = table.Column<string>(maxLength: 100, nullable: true),
+                    Created = table.Column<DateTime>(nullable: false),
+                    IsAdmin = table.Column<bool>(nullable: false),
+                    IsActive = table.Column<bool>(nullable: false),
+                    ValidationKey = table.Column<string>(maxLength: 35, nullable: true),
+                    Password = table.Column<string>(maxLength: 80, nullable: false)
                 },
                 constraints: table =>
                 {
@@ -94,9 +124,9 @@ namespace KavaDocsUserManager.Business.Migrations
                 columns: table => new
                 {
                     Id = table.Column<Guid>(nullable: false),
-                    IsOwner = table.Column<bool>(nullable: false),
-                    RepositoryId = table.Column<Guid>(nullable: false),
-                    UserId = table.Column<Guid>(nullable: false)
+                    UserTypes = table.Column<int>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false),
+                    RepositoryId = table.Column<Guid>(nullable: false)
                 },
                 constraints: table =>
                 {
@@ -109,6 +139,38 @@ namespace KavaDocsUserManager.Business.Migrations
                         onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
                         name: "FK_UserRepositories_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRoles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false),
+                    RoleId = table.Column<Guid>(nullable: false),
+                    RepositoryId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRoles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserRoles_Repositories_RepositoryId",
+                        column: x => x.RepositoryId,
+                        principalTable: "Repositories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserRoles_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserRoles_Users_UserId",
                         column: x => x.UserId,
                         principalTable: "Users",
                         principalColumn: "Id",
@@ -135,10 +197,28 @@ namespace KavaDocsUserManager.Business.Migrations
                 name: "IX_UserRepositories_UserId",
                 table: "UserRepositories",
                 column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_RepositoryId",
+                table: "UserRoles",
+                column: "RepositoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_RoleId",
+                table: "UserRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_UserId",
+                table: "UserRoles",
+                column: "UserId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "AuthTokens");
+
             migrationBuilder.DropTable(
                 name: "OrganizationRepositories");
 
@@ -146,10 +226,16 @@ namespace KavaDocsUserManager.Business.Migrations
                 name: "UserRepositories");
 
             migrationBuilder.DropTable(
+                name: "UserRoles");
+
+            migrationBuilder.DropTable(
                 name: "Organizations");
 
             migrationBuilder.DropTable(
                 name: "Repositories");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
 
             migrationBuilder.DropTable(
                 name: "Users");
